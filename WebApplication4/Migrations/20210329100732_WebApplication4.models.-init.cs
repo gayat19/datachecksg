fda2c1f58ac9bbe0b2d4d282971e3ca14d9102b7
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-03-29 15:41:02 +0530

    Ïnitial commit

---
+++ WebApplication4/Migrations/20210329100732_WebApplication4.models.-init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication4.Migrations
+{
+    public partial class WebApplication4modelsinit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 101, "Ramu" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
---
+++ WebApplication4/Models/CompanyContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions options):base(options)
+        {
+                
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                Id = 101,
+                Name= "Ramu"
+            });
+        }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
